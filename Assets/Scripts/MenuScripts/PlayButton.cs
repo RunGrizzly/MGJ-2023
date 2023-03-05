cd6f69b384660aa6d75a7867b23a02dd4d1fb765
Eeeeeeeeeeeeen <ianthomson92@hotmail.co.uk>
2023-03-05 21:34:59 +0000

    Saved Menu Scene...

--- Assets/Scripts/MenuScripts/PlayButton.cs
+++ Assets/Scripts/MenuScripts/PlayButton.cs
@@ -5,36 +5,11 @@ public class PlayButton : MonoBehaviour
 {
     public TMP_Text text;
 
-    private float m_minSize;
-
-    private float m_maxSize;
-
-    private bool m_isIncreasing = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_minSize = text.fontSize - 30;
-        m_maxSize = text.fontSize + 30;
-    }
-
+    [SerializeField] private float maxSize = 0.25f;
     // Update is called once per frame
     void Update()
     {
-        if (m_isIncreasing)
-        {
-            text.fontSize += 50 * Time.deltaTime;
-            if(text.fontSize >= m_maxSize)
-            {
-                m_isIncreasing = !m_isIncreasing;
-            }
-        }
-        else
-        {
-            text.fontSize -= 50 * Time.deltaTime;
-            if(text.fontSize <= m_minSize)
-            {
-                m_isIncreasing = !m_isIncreasing;
-            }
-        }
+        text.transform.localScale = Vector3.one + Vector3.one * (maxSize * Mathf.Sin(Time.time));
     }
+
 }
