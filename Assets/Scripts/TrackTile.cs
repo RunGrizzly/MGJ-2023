88437de0ae2da49c590cc34b7d6fd71626026fea
Ian Thomson <ianthomson92@gmail.com>
2023-03-10 22:10:08 +0000

    Basic Grid stuff

--- Assets/Scripts/TrackTile.cs
+++ Assets/Scripts/TrackTile.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackTile : MonoBehaviour
+public class TrackTile: MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TileState m_state;
+    private Vector2Int m_position;
+
+    public TrackTile()
     {
-        
+        m_state = TileState.Empty;
+        //Using -1 -1 to say we're "off-grid"
+        m_position = new Vector2Int(-1, -1);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SetTileMesh();
     }
+
+    public void SetState(TileState tileState, Vector2Int position)
+    {
+        if (!Equals(m_state, tileState))
+        {
+            m_state = tileState;
+            SetTileMesh();
+        }
+
+        m_position = position;
+    }
+
+    private void SetTileMesh()
+    {
+        Renderer renderer = gameObject.GetComponent<Renderer>();
+
+        switch (m_state)
+        {
+            case TileState.Empty:
+                renderer.material.color = Color.white;
+                break;
+            case TileState.Track:
+                renderer.material.color = Color.black;
+                break;
+        }
+    }
+}
+
+public enum TileState {
+    Empty,
+    Track
 }
