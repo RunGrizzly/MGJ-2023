2be5f3883366c43cd5dcc0405aaa64751aeb67ed
SI\Joshua.Henderson <joshua.henderson@sigames.com>
2023-03-12 01:30:02 +0000

    Environment tweaks. Clouds. Name properly looking at camera.

--- Assets/LookAtCamera.cs
+++ Assets/LookAtCamera.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class LookAtCamera : MonoBehaviour
 {
-    void Update()
+    public float m_rotateSpeed = 1.5f;
+    public Quaternion m_offset;
+
+    void FixedUpdate()
     {
-        transform.LookAt(Camera.main.transform, Vector3.up);
+        transform.LookAt(Camera.main.transform);
+        transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
     }
 }
